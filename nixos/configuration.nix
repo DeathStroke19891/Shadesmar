ce6921adb4cf8423f619c6fbb71405c345890840
Sridhar Kedlaya <kedlayasridhar@gmail.com>
2026-02-21 18:02:43 +0000

    Update system: Gen 12 - 2026-02-21 18:02:40 - 26.05.20260216.d1c15b7 - 6.18.10

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -110,6 +110,20 @@
         proxyWebsockets = true;
       };
     };
+    virtualHosts."misskey.sridharkedlaya.xyz" = {
+      enableACME = true;
+      forceSSL = true;
+      locations."/" = {
+        proxyPass = "http://127.0.0.1:3000";
+        proxyWebsockets = true; # needed if you need to use WebSocket
+        # extraConfig =
+        #   # required when the target is also TLS server with multiple hosts
+        #   "proxy_ssl_server_name on;" +
+        #   # required when the server wants to use HTTP Authentication
+        #   "proxy_pass_header Authorization;"
+        #   ;
+      };
+    };
   };
 
   networking.firewall.allowedTCPPorts = [80 443];
